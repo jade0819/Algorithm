4dcef831460bdc89619a8e2a91d69a025768385c
jade0819 <break0819@gmail.com>
2022-02-17 22:30:54 +0900

    Algorithm I/O

---
+++ 단계별/입출력과 사칙연산/Test11.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Test11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() - 543);
+    }
+}
