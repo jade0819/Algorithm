d2039094cfd7402c3b9c944893c86bc73effb270
jade0819 <break0819@gmail.com>
2022-03-22 22:36:11 +0900

    for 6, 7번

---
+++ 단계별/for문/Test6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Test6 {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        for (int i = n; i > 0; i--)
+            System.out.println(i);
+    }
+}
